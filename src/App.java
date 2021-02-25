a52c7382523077ae80b52f6f024fe15af580fef6
Paul <hughpaud@gmail.com>
2021-02-25 13:48:13 -0600

    :books: Updated License and Source Code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+/* 
+    Here is another short example using variables.
+    Call this file App.java
+*/
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int num; // This declares a variable called num.
+        num = 100; // This assigns num the value 100.
+
+
+        System.out.println("This is num: " + num);
+
+        num = num * 2;
+        System.out.print("The value of num * 2 is ");
+        System.out.println(num);
     }
 }
